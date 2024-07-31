6625d033b31c4c0351bf87334da69624ac708833
Aryanask16 <166402806+Aryanask16@users.noreply.github.com>
2024-07-31 11:55:14 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="hero-section mt-20">
+      <div className=" mt-20">
       <Hero />
       </div>
-      <div className="about-section mt-20">
+      <div className=" mt-20">
       <About />
       </div>
       <Services />
