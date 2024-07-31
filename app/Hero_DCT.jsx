eac75bf44555127941b7b2f37066b3b11cdfcae0
Aryanask16 <166402806+Aryanask16@users.noreply.github.com>
2024-07-31 11:56:09 +0530

    Update Hero_DCT.jsx

--- app/Hero_DCT.jsx
+++ app/Hero_DCT.jsx
@@ -7,7 +7,7 @@ import Vortex from "@/components/ui/Vortox";
 
 export default function Hero() {
   return (
-    <div className="w-[calc(100%-4rem)] mx-auto rounded-md h-[30rem] overflow-hidden">
+    <div className="w-[calc(100%-4rem)] mx-auto rounded-md h-[30rem] overflow-hidden mt-20">
       <Vortex
         backgroundColor="black"
         className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
