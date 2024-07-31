0733e2f7c59bfd577c927a13a36a2a8465ac80ad
Aryanask16 <166402806+Aryanask16@users.noreply.github.com>
2024-07-31 11:57:16 +0530

    Update About_DCT.jsx

--- app/About_DCT.jsx
+++ app/About_DCT.jsx
@@ -5,7 +5,7 @@ import {cn} from "@/lib/utils";
 
 export default function About() {
   return (
-    <div className="h-96 relative w-full mb-24 overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg" id="contact">
+    <div className="h-96 mt-20 relative w-full mb-24 overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg" id="contact">
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       <Boxes />
