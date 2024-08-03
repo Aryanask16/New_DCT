85e33a11c1c3e42ccc46e6d3ae007f4af060b0ae
SANS-1104 <sanskriti020803@gmail.com>
2024-08-03 12:12:27 +0530

    commit_sanskriti1

--- components/ui/background-boxes.jsx
+++ components/ui/background-boxes.jsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import {cn} from "@/lib/utils";
-// import { cn } from "@/utils/cn";
+import { cn } from "@/lib/utils";
 
 export const BoxesCore = ({ className, ...rest }) => {
   const rows = new Array(150).fill(1);
@@ -28,27 +27,24 @@ export const BoxesCore = ({ className, ...rest }) => {
         transform: `translate(-40%,-60%) skewX(-48deg) skewY(14deg) scale(0.675) rotate(0deg) translateZ(0)`,
       }}
       className={cn(
-        "absolute left-1/4 p-4 -top-1/4 flex  -translate-x-1/2 -translate-y-1/2 w-full h-full z-0 ",
-        className
+        "absolute left-1/4 p-4 -top-1/4 flex -translate-x-1/2 -translate-y-1/2 w-full h-full z-0",
+        className // Ensure className is correctly passed
       )}
       {...rest}
     >
       {rows.map((_, i) => (
         <motion.div
           key={`row` + i}
-          className="w-16 h-8  border-l  border-slate-700 relative"
+          className="w-16 h-8 border-l border-slate-700 relative"
         >
           {cols.map((_, j) => (
             <motion.div
               whileHover={{
                 backgroundColor: `var(${getRandomColor()})`,
-                transition: { duration: 0 },
-              }}
-              animate={{
-                transition: { duration: 2 },
+                transition: { duration: 0.2 },
               }}
               key={`col` + j}
-              className="w-16 h-8  border-r border-t border-slate-700 relative"
+              className="w-16 h-8 border-r border-t border-slate-700 relative"
             >
               {j % 2 === 0 && i % 2 === 0 ? (
                 <svg
